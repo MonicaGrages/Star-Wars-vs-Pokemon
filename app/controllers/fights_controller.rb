8e8b7b5b58b369ae989d969fdd5cc719169f3405
MonicaGrages <mgrages1@gmail.com>
2017-06-26 12:51:19 -0400

    add leaderboard

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -2,6 +2,7 @@ class FightsController < ApplicationController
   def index
     @star_war = StarWar.generate rand(90)
     @pokemon = Pokemon.generate rand(719)
-    @star_wars = StarWar.order(wins: :desc).limit(3)
+    @star_wars = StarWar.order(wins: :desc).limit(5)
+    @pokemons = Pokemon.order(wins: :desc).limit(5)
   end
 end
